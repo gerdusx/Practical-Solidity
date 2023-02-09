f784467fca1c254c682afe700a3aa99879348ba1
Gerdus Marais <gerdusx@gmail.com>
2023-02-09 22:39:04 +0200

    Fixed rate staking init

---
+++ staking/src/FixedRate.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract FixedRate {
+
+
+    function stake() public {
+
+    }
+
+    function unstake() public {
+
+    }
+
+    function claim() public {
+
+    }
+
+}
